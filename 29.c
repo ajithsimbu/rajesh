23a2af4666c3251fc624effe2170dcdde5857349
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2018-01-21 16:16:39 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int numMinutes = 103;
+
+int main ( void )
+{
+
+printf("%01d hour",numMinutes/60);
+else /* display 'x' hours .... */
+printf("%01d hours",numMinute
+printf(" %01d minute\n",numMinutes%60);
+else /* display 'x' minutes....*/
+printf(" %01d minutes\n",numMinutes%60);
+
+return 0;
+}
