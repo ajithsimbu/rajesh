a15b55bd52c4b89aa705eeea1c26c44071f8f87c
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2018-01-21 16:11:12 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[30];
+int i,flag=0,n=0;
+clrscr();
+printf("enter the string:");
+scanf("%s",s)
+{
+{
+flag=flag+1;
+
+printf("given string not contains numbers");
+else
+printf("given string contains numbers");
+getch();
+}
