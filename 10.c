3b912582b63751f5bc399a025a6035f6621006a8
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2018-01-21 14:22:01 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int addNum(int n);
+int main()
+{
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    printf("Sum = %d",addNum(num));
+    return 0;
+}
+int addNum(int n)
+{
+    if(n != 0)
+        return n + addNum(n-1);
+    else
+        return n;
+}
