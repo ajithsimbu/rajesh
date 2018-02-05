4dc936b267622f21bbab8990fc40340501a5aabc
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2018-02-05 16:37:15 +0530

    Create 37.c

---
+++ 37.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=5,b=8,t;
+    printf("value of 'a' and 'b' before swaping%d%d\n",a,b);
+    t=a;
+    a=b;
+    b=t;
+    printf("value of 'a' and 'b' after swaping%d%d\n",a,b);
+    return 0;
+}
