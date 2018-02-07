fa95e40080867102b0d693e4bd6dcc40be9ed9f1
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2018-02-07 15:50:16 +0530

    Create 38.c

---
+++ 38.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int v,max,m;
+scanf("%d",&m);
+printf("enter the number");
+for(i=0;i<=m;i++)
+scanf("%d",&a[i]);
+max=a[i];
+if(max<a[i])
+{
+max=a[i];
+}
+printf("%d",max);
+}
