76b4103c97cb557531593d067ccd98feb208ef73
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2018-02-07 15:53:53 +0530

    Create 40.c

---
+++ 40.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+	int n,a=0,b=1,t,i;
+	printf("enter the number");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		printf("%d",t1);
+		t=a+b;
+		a=b;
+    b=t;
+	}
+return 0;
+}
