03ab9aa86d3f62be2e27d342a53be43bd663a92f
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2018-02-09 14:43:09 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,r,s;
+char a[30]b[50];
+r=strlen(a);
+s=strlen(b);
+for(i=0;i<n;i++)
+{
+if(a[i]>b[i])
+{
+printf("a is big");
+}
+}
+else
+{
+if(a[i]<b[i])
+{
+printf("b is big");
+}
+}
+printf("%s%s",a,b);
+}
