1082eaacaaed8f9f70350da53ed5ca67e3e55727
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2018-02-12 16:10:49 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num=1,i;
+printf("enter the number");
+scanf("%d",num);
+for(i=0;i<10;i++)
+if(n||1)
+printf("print the num");
+else
+printf("don't print the number");
+}
+
+
