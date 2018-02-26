c68893353c2fa3a3e2ad08085515d51cfdfc5868
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2018-02-26 16:41:48 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    switch(n)
+    {
+    case 1:
+    printf("one");
+    break;
+    case 2:
+    printf("two");
+    break;
+    case 3:
+    printf("three");
+    break;
+    default:
+    printf("error");
+    }
+
+}
+
+
