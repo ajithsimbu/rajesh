ca4c85eccb4589b02f3148a27d109780fa797436
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2018-03-02 16:04:51 +0530

    Update 57.c

--- 57.c
+++ 57.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 void main()
 {
-   int x,y,temp;
-printf("Enter value of x and y\n");
- scanf("%d%d", &x, &y);
-   printf("before Swapping\n%d\n%d\n",x,y);
-   temp=x;
-   x=y;
-   y=temp;
-   printf("after Swapping\n%d\n%d\n",x,y);
+   int a,b,temp;
+ scanf("%d%d", &a, &b);
+   printf("before Swapping\n%d\n%d\n",a,b);
+   temp=a;
+   a=b;
+   b=temp;
+   printf("after Swapping\n%d\n%d\n",a,b);
   
 }
+
