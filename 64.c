cb8b0b301383ce0dc59291c18bbdefde5d4d6345
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2018-03-12 16:10:47 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a,b; 
+   printf("enter two numbers");
+   scanf("%d",&a);
+ scanf("%d",&b);
+ if((a+b)%2==0)
+ printf("even");
+ else
+ printf("odd");
+}
